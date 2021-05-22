d93d18096935d6f75dcd8afe213af452e88e947b
Alexander Fatyuhin <virikidorhom@gmail.com>
2021-05-22 09:09:44 +0300

    шаблоны приведены к использованию слоёв

---
+++ resources/views/Article/links_redact_delete.blade.php
@@ -0,0 +1,4 @@
+<div class="links">
+    <a class="button" href="{{route('article.edit', ['article' => $article])}}" >редактировать</a>
+    <a class="button" href="{{route('article.delete', ['article' => $article])}}" >удалить</a>
+</div>
