de82fa40adb79d01bd4b99ae3b4e6d21a6de2574
Alexander Fatyuhin <virikidorhom@gmail.com>
2021-05-22 20:58:26 +0300

    отформатировал вывод даты

--- resources/views/Article/index.blade.php
+++ resources/views/Article/index.blade.php
@@ -13,7 +13,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <h3><a href="{{route('article.show', ['article' => $article])}}" >{{$article->title}}</a></h3>
-                <h6> date: {{$article->created_at}}</h6>
+                <h6> date: {{$article->created_at->format('Y-m-d')}}</h6>
                 <div class="">{{$article->text}} </div>
 
                 @include('article.links_redact_delete')
--- resources/views/Article/show.blade.php
+++ resources/views/Article/show.blade.php
@@ -12,6 +12,7 @@
 
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
         <h1>{{$article->title}}</h1>
+        <h6> date: {{$article->created_at->format('Y-m-d')}}</h6>
         <div class="text_formated">{{$article->text}}</div>
 
         @include('article.links_redact_delete')
